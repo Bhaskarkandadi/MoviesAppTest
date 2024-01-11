b51cba2a09be137188adb1fb62ff5550506b8771
Bhaskarkandadi <156230236+Bhaskarkandadi@users.noreply.github.com>
2024-01-11 11:46:56 +0530

    Update HomePage.java

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -44,7 +44,6 @@ public class HomePage {
         return headingElement.getText();
     }
     public boolean areMoviesDisplayedInSection(String sectionName) {
-        // Assuming each movie section has a unique identifier
         return waitForVisibilityOfElement(moviesSection).isDisplayed() &&
                 !driver.findElements(moviesSection).isEmpty();
     }
